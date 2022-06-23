9a01a871d5a1dec79d4bfea469af1afe45e7dc99
Ronny Trommer <ronny@opennms.org>
2022-06-23 19:49:53 +0200

    Update CircleCI base image to 22.04

--- version-lock.sh
+++ version-lock.sh
@@ -7,8 +7,8 @@ DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 export VCS_SOURCE
 export VCS_REVISION
 export DATE
-export BASE_IMAGE="cimg/base:2022.03-20.04"
-export SHELLCHECK_VERSION="0.7.0-2build2"
-export HADOLINT_VERSION="2.8.0"
-export GETTEXT_VERSION="0.19.8.1-10build1"
+export BASE_IMAGE="cimg/base:2022.06-22.04"
+export SHELLCHECK_VERSION="0.8.0-2"
+export HADOLINT_VERSION="2.10.0"
+export GETTEXT_VERSION="0.21-4ubuntu4"
 
