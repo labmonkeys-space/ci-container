2eddf643520395251377fb6546c228a835e1c9ed
Ronny Trommer <ronny@opennms.org>
2023-06-15 13:23:03 +0200

    Upate base image

--- version-lock.sh
+++ version-lock.sh
@@ -7,7 +7,7 @@ DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 export VCS_SOURCE
 export VCS_REVISION
 export DATE
-export BASE_IMAGE="cimg/base:2023.03-22.04"
+export BASE_IMAGE="cimg/base:2023.06-22.04"
 export SHELLCHECK_VERSION="0.8.0-2"
 export HADOLINT_VERSION="2.10.0"
 export GETTEXT_VERSION="0.21-4ubuntu4"
